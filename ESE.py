597faebdfb24a33278256a27d0dd6c4e008d2e93
andreuva <andres.vicente.arevalo@gmail.com>
2021-04-12 17:22:10 +0100

    added the base classes from Jiri

---
+++ ESE.py
@@ -0,0 +1,36 @@
+class ESE:
+    """ A class that stores the atomic state and needs to be constantly updated
+        during the Lambda iterations by providing the Stokes parameters.
+        After every Lambda iteration, solveESE() needs to be called.
+        It is assumed that only one spectral line is involved in the problem.
+        This class needs to be instantiated at every grid point.
+    """
+
+    def __init__(self, nu_quad_w, B):
+        """ 
+            nu_quad: array of the frequency quadrature weights
+            B: object of the magnetic field vector with .x, .y, .z components (in gauss)
+            return value: None
+        """
+        pass
+    
+    def addStokes(self, stokes, ray, ray_w):
+        """ Called per every Lambda iteration, grid point, and ray direction.
+            stokes: list of 4 arrays of Stokes parameters [I,Q,U,V] in given point and direction (all frequencies)
+            ray: object with .theta and .chi variables defining the ray of propagation direction
+            ray_w: direction quadrature weight of the ray
+            return value: None
+        """
+        pass
+    
+    def solveESE(self):
+        """ Called at every grid point at the end of the Lambda iteration.
+            return value: maximum relative change of the level population
+        """
+        pass
+    
+    def resetRadiation(self):
+        """ Called at the beginning of every Lambda iteration.
+            It initializes the internal state.
+        """
+        pass
---
+++ RTcoefs.py
@@ -0,0 +1,20 @@
+class RTcoefs:
+    """ Radiative transfer coefficients.
+        Just one instance of the class needs to be created in the program.
+    """
+
+    def __init__(self, nus):
+        """ 
+            nus: array of the line frequencies
+            return value: None
+        """
+        pass
+
+    def getRTcoefs(self, ese, ray):
+        """ Provides the 4-vector of epsilon and the 4x4 K-matrix for the point
+            with given ESE state and ray direction.
+            ese: the local instance of the ESE class
+            ray: object with .theta and .chi variables defining the ray of propagation direction
+            return value: [epsilon (vector in frequencies), K (4x4 list of vectors in frequencies)]
+        """
+        pass
