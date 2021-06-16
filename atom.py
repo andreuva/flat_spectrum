corrected normalization voigt and Jqq computation

atom.py
@@ -83,6 +83,8 @@ class ESE:
             return value: None
         """
 
+        self.nus_weights = nus_weights
+        self.B = B
         self.atom = HeI_1083()
         self.rho = np.zeros(len(self.atom.dens_elmnt))
         self.populations = 0
@@ -134,20 +136,20 @@ class ESE:
                     if Lj > Li:
                         # calculate the TE(q -> p) and add it to self.ESE[i][j]
                         self.ESE[i][j] = self.ESE[i][j] + TE(self, JJ, M, Mp, Jp, N, Np, line.A_lu)\
-                                                        + TS(self, JJ, M, Mp, Jp, N, Np, rad, line.B_ul, line.nu)
+                                                        + TS(self, JJ, M, Mp, Jp, N, Np, rad, line.B_ul, cdt)
                     elif Lj < Li:
                         # calculate the TA(q -> p) and add it to self.ESE[i][j]
-                        self.ESE[i][j] = self.ESE[i][j] + TA(self, JJ, M, Mp, Jp, N, Np, rad, line.B_lu, line.nu)
+                        self.ESE[i][j] = self.ESE[i][j] + TA(self, JJ, M, Mp, Jp, N, Np, rad, line.B_lu, cdt)
                     elif Lj == Li:
                         # calculate the RA and RE
                         if M == N:
-                            self.ESE[i][j] = self.ESE[i][j] - (RA(self, Li, JJ, Mp, Np, rad) +
+                            self.ESE[i][j] = self.ESE[i][j] - (RA(self, Li, JJ, Mp, Np, rad, cdt) +
                                                                RE(self, Li, JJ, Np, Mp) +
-                                                               RS(self, Li, JJ, Np, Mp, rad))
+                                                               RS(self, Li, JJ, Np, Mp, rad, cdt))
                         if Mp == Np:
-                            self.ESE[i][j] = self.ESE[i][j] - (RA(self, Li, JJ, N, M, rad) +
+                            self.ESE[i][j] = self.ESE[i][j] - (RA(self, Li, JJ, N, M, rad, cdt) +
                                                                RE(self, Li, JJ, M, N) +
-                                                               RS(self, Li, JJ, M, N, rad))
+                                                               RS(self, Li, JJ, M, N, rad, cdt))
                         else:
                             continue
                     else:
@@ -204,14 +206,14 @@ class ESE:
 
 
 # Eq 7.9 from LL04 for the SEE coeficients
-def TA(ESE, J, M, Mp, Jl, Ml, Mlp, rad, Blu, nu):
+def TA(ESE, J, M, Mp, Jl, Ml, Mlp, rad, Blu, cdt):
     # Applied selection rules to remove sumation
     q = int(Ml - M)
     qp = int(Mlp - Mp)
 
     sum_qq = 3*(-1)**(Ml - Mlp)*(ESE.jsim.j3(J, Jl, 1, -M,  Ml, -q) *
                                  ESE.jsim.j3(J, Jl, 1, -Mp, Mlp, -qp) *
-                                 rad.Jqq_nu(q, qp, nu))
+                                 rad.Jqq_nu(cdt, line, q, qp, M, Ml, ESE.B, ESE.nus_weigths))
     return (2*Jl + 1)*Blu*sum_qq
 
 
@@ -223,18 +225,18 @@ def TE(ESE, J, M, Mp, Ju, Mu, Mup, Aul):
     return (2*Ju + 1)*Aul*sum_q
 
 
-def TS(ESE, J, M, Mp, Ju, Mu, Mup, rad, Bul, nu):
+def TS(ESE, J, M, Mp, Ju, Mu, Mup, rad, Bul, cdt):
     # Applied selection rules to remove sumation
     q = int(Mp - Mup)
     qp = int(M - Mu)
 
     sum_qq = 3*(-1)**(Mp - M)*(ESE.jsim.j3(Ju, J, 1, -Mup, Mp, -q) *
                                ESE.jsim.j3(Ju, J, 1, -Mu,  M, -qp) *
-                               rad.Jqq_nu(q, qp, nu))
+                               rad.Jqq_nu(cdt, line, q, qp, Mu, M, ESE.B, ESE.nus_weigths))
     return (2*Ju + 1)*Bul*sum_qq
 
 
-def RA(ESE, Li, J, M, Mp, rad):
+def RA(ESE, Li, J, M, Mp, rad, cdt):
 
     sum_u = 0
     for k, k_lev in enumerate(ESE.atom.levels):
@@ -254,7 +256,7 @@ def RA(ESE, Li, J, M, Mp, rad):
                         for Mu in Mus:
                             sum_qqMu += 3*(-1)**(M - Mp)*(ESE.jsim.j3(Jk, J, 1, -Mu, M, -q) *
                                                           ESE.jsim.j3(Jk, J, 1, -Mu, Mp, -qp) *
-                                                          rad.Jqq_nu(q, qp, line.nu))
+                                                          rad.Jqq_nu(cdt, line, q, qp, Mu, M, ESE.B, ESE.nus_weigths))
                 sum_u += (2*J+1)*line.B_lu*sum_qqMu
 
     return 0.5*sum_u
@@ -278,7 +280,7 @@ def RE(ESE, Li, J, M, Mp):
     return 0.5*sum_l
 
 
-def RS(ESE, Li, J, M, Mp, rad):
+def RS(ESE, Li, J, M, Mp, rad, cdt):
 
     sum_l = 0
     for k, k_lev in enumerate(ESE.atom.levels):
@@ -298,7 +300,7 @@ def RS(ESE, Li, J, M, Mp, rad):
                         for Ml in Mls:
                             sum_qqMl += 3*(ESE.jsim.j3(J, Jk, 1, -M, Ml, -q) *
                                            ESE.jsim.j3(J, Jk, 1, -Mp, Ml, -qp) *
-                                           rad.Jqq_nu(q, qp, line.nu))
+                                           rad.Jqq_nu(cdt, line, q, qp, M, Ml, ESE.B, ESE.nus_weigths))
 
                 sum_l += (2*J+1)*line.B_ul*sum_qqMl
 

conditions.py
@@ -98,6 +98,7 @@ class conditions:
         self.v_dop = parameters.v_dop
         self.a_voigt = parameters.a_voigt
         self.n_dens = parameters.n_dens
+        self.mass = 4.002602 * 1.6605402e-24 * units.g
         self.temp = parameters.temp
 
         # Initialice the array of the magnetic field vector
@@ -114,9 +115,18 @@ class conditions:
         vs = self.nus.value
         v0 = line.nu.value + (norm(B)/constants.h.cgs.value *
                               (line.gu*Mu - line.gl*Ml))
-        profile = voigt(vs-v0, self.a_voigt)
-        normalization = np.sum(profile*self.nus_weights)
-        return profile/normalization
+        vt = np.sqrt(constants.k_B.cgs*self.temp/self.mass).decompose().cgs
+        delt_v = line.nu*vt/constants.c.cgs
+        profile = voigt((vs-v0)/delt_v, self.a_voigt)
+
+        profile.imag = profile.imag / (np.sqrt(np.pi)*delt_v)
+        profile.real = profile.real / (np.sqrt(np.pi)*delt_v)
+
+        normalization = np.sum(profile.real*self.nus_weights)
+
+        profile.real = profile.real/normalization
+
+        return profile
 
 
 class state:

rad.py
@@ -59,6 +59,7 @@ class RTE:
             for qp in [-1, 0, 1]:
                 self.jqq[qq][qp] = self.jqq[qq][qp] * 0
 
-    def Jqq_nu(self, q, qp, nu):
+    def Jqq_nu(self, cdt, line, q, qp, Mu, Ml, B, nus_weigths):
         jqqp = self.jqq[q][qp]
-        return np.interp(nu, self.nus, jqqp).value
+        profile = cdt.voigt_profile(line, Mu, Ml, B)
+        return np.sum(jqqp*profile*nus_weigths).value