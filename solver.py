ba9fb81c65ff41426ce6567ed7e0d038b506b079
andreuva <andres.vicente.arevalo@gmail.com>
2021-07-05 15:04:03 +0100

    changed Km - ko in solver

--- solver.py
+++ solver.py
@@ -11,21 +11,21 @@ def BESSER(point_M, point_O, point_P, sf_m, sf_o, sf_p, kk_m, kk_o, kk_p, ray, c
     k_o = np.moveaxis(np.diagonal(kk_o, 0, 0, 1), 0, -1).copy()
 
     tauMO = ((k_m + k_o)/2) * np.abs((point_O.z - point_M.z)/np.cos(ray.inc)) + 1e-30
-    tau_tot = np.append(tau_tot, tau_tot[-1] + tauMO[0][79])
+    tau_tot = np.append(tau_tot, tau_tot[-1] + tauMO[0][2])
     tauOP = ((k_o + k_p)/2) * np.abs((point_P.z - point_O.z)/np.cos(ray.inc)) + 1e-30
-
-    # Compute the psi_m and psi_o
-    to_taylor_psi = tauMO < 1e-3
 
     exp_tauMO = np.exp(-np.where(tauMO < 700, tauMO, 700))
 
-    u_0 = 1 - exp_tauMO
-    for i, disc in enumerate(to_taylor_psi):
-        u_0[i][disc] = tauMO[i][disc] - tauMO[i][disc]**2/2 + tauMO[i][disc]**3/6
-    u_1 = tauMO - u_0
+    # Compute the psi_m and psi_o
+    # to_taylor_psi = tauMO < 1e-3
 
-    psi_m = u_0 - u_1/tauMO
-    psi_o = u_1/tauMO
+    # u_0 = 1 - exp_tauMO
+    # for i, disc in enumerate(to_taylor_psi):
+    #     u_0[i][disc] = tauMO[i][disc] - tauMO[i][disc]**2/2 + tauMO[i][disc]**3/6
+    # u_1 = tauMO - u_0
+
+    # psi_m = u_0 - u_1/tauMO
+    # psi_o = u_1/tauMO
 
     psi_m = (1 - exp_tauMO*(1 + tauMO))/(tauMO)
     psi_o = (exp_tauMO + tauMO - 1)/(tauMO)
@@ -68,15 +68,15 @@ def BESSER(point_M, point_O, point_P, sf_m, sf_o, sf_p, kk_m, kk_o, kk_p, ray, c
 
     # BESSER INTERPOLATION Jiri Stepan A&A 2013
     # Step 1: calculate dm(p) = (y0(p) - ym(0))/hm(p)
-    cm, cp = BESSER_interp(tauMO, tauOP, sf_m, sf_o, sf_p)*I_units
+    cm = BESSER_interp(tauMO, tauOP, sf_m, sf_o, sf_p)*I_units
 
-    k_1_inv = (cdt.Id_tens*kk_m.unit + psi_o*kk_m)
+    k_1_inv = (cdt.Id_tens*kk_o.unit + psi_o*kk_o)
 
     # Inverting the matrices K^-1 for all the wavelenghts
     k_1 = np.zeros_like(k_1_inv)/k_1_inv.unit**2
     for k in range(cdt.nus_N):
-        k_1[:, :, k] = np.linalg.solve(k_1_inv[:, :, k], cdt.identity)
-    k_2 = (exp_tauMO*kk_o.unit - psi_m*kk_o)
+        k_1[:, :, k] = np.linalg.inv(k_1_inv[:, :, k])
+    k_2 = (exp_tauMO*kk_m.unit - psi_m*kk_m)
     # Multipling matrices of all wavelengths with at once (eq 7 and 8)
     k_2 = np.einsum("ijb, jkb -> ikb", k_1, k_2)
     kt = np.einsum("ijk, jk -> ik", k_2, point_M.radiation.stokes)
@@ -150,4 +150,4 @@ def BESSER_interp(tauMO, tauOP, sf_m, sf_o, sf_p):
                     # Step 9: Calculate the new cm to keep the derivative smooth
                     cm[i, w] = sf_o[i, w] - dy*hm[i, w]/2
 
-    return cm, cp
+    return cm
