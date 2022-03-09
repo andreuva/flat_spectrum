a435a89fa6f93805855676a7ccb6e09a681509f3
Andres Vicente <andres.vicente.arevalo@gmail.com>
2022-03-09 15:01:22 +0100

    changed debug to verbose and supressed outputs

--- main.py
+++ main.py
@@ -40,143 +40,26 @@ def main():
         if not os.path.isdir(datadir+fil):
             os.remove(datadir+fil)
 
-    #############
-    # Debug SEE #
-    #############
-   #if True:
-    if False:
-
-        # Import extras
-        from physical_functions import jsymbols
-        JS = jsymbols()
-
-        '''
-        def fffu(Jl,Ml,Ju,Mu):
-            ww = JS.j3(Ju,Jl,1,-Mu,Ml,Mu-Ml)
-            return (2.*Ju+1.)*ww*ww
-        def fffl(Jl,Ml,Ju,Mu):
-            ww = JS.j3(Ju,Jl,1,-Mu,Ml,Mu-Ml)
-            return (2.*Jl+1.)*ww*ww
-
-        jls = [1]
-        jus = [2,1,0]
-        for jl in jls:
-            mls = np.linspace(-jl,jl,2*jl+1,endpoint=True)
-            for mll in mls:
-                ml = int(round(mll))
-                for ju in jus:
-                    mus = np.linspace(-ju,ju,2*ju+1,endpoint=True)
-                    for muu in mus:
-                        mu = int(round(muu))
-                        if np.absolute(mu-ml) > 1:
-                            continue
-                        print(f'({jl:1d}{ml:2d},{ju:1d}{mu:2d}) = ' + \
-                              f'{fffu(jl,ml,ju,mu)} {fffl(jl,ml,ju,mu)}')
-        sys.exit()
-        '''
-
-        # Define a point
-        pointC = point(st.atomic[1], st.radiation[1], cdt.zz[1])
-
-        # Ad-hoc radiation field
-        JKQ = {0: {0: 3e0 + 0j}, \
-               1: {0: 0. + 0.j, \
-                   1: 0. + 0.j}, \
-               2: {0: 0. + 0.j, \
-                   1: 0. + 0.j, \
-                   2: 0. + 0.j}}
-
-        # Get negative Q
-        for K in range(3):
-            for Q in range(1,K+1):
-                if Q == 1:
-                    ss = -1.0
-                elif Q == 2:
-                    ss = 1.0
-                JKQ[K][-Q] = ss*np.conjugate(JKQ[K][Q])
-
-        # Add units to JKQ
-        for K in range(3):
-            for Q in range(-K,K+1):
-                JKQ[K][Q] = JKQ[K][Q]
-
-        # Get Jqq (assuming Helium)
-        line = pointC.atomic.atom.lines[0]
-
-        # For each q
-        for qq in range(-1,2):
-
-            # Factor for q
-            f1 = JS.sign(1+qq)
-
-            # Initialize jqq
-            line.jqq[qq] = {}
-
-            # For each q'
-            for qp in range(-1,2):
-
-                # Initialize
-                line.jqq[qq][qp] = (0. + 0.j)
-
-                # For each K
-                for K in range(3):
-
-                    # K factor
-                    f2 = f1*np.sqrt((2.*K+1.)/3.)
-
-                    # Get Q from 3J
-                    Q = qq - qp
-
-                    # Control Q
-                    if np.absolute(Q) > K:
-                        continue
-
-                    # Contribution
-                    contr = f2*JS.j3(1,1,K,qq,-qp,-Q)*JKQ[K][Q]
-
-                    # Add contribution
-                    line.jqq[qq][qp] += contr
-
-        # Ad-hoc Einstein
-       #line.A_ul /= line.A_ul
-       #line.B_ul /= line.B_ul
-       #line.B_lu = line.B_ul * (pointC.atomic.atom.terms[1].g/ \
-       #                         pointC.atomic.atom.terms[0].g)
-       #line.B_ul *= 0.
-       #for i in range(20):
-       #    print('NO STIMULATED')
-       #line.B_lu /= line.B_lu
-
-        # Summon SEE
-        pointC.atomic.solveESE(st.radiation[1],cdt)
-
-        # Exit debug
-        sys.exit('Debugging SEE')
-    #############
-    # Debug SEE #
-    #############
-
     # Plot quadrature
-   #plot_quadrature(cdt, directory=pm.dir)
+    #plot_quadrature(cdt, directory=pm.dir)
 
-    # Debug
-    debug = False
+    # verbose
+    verbose = False
 
     # Load data
     # ''
     # TODO TODO TODO TODO TODO
 
     # Not loading
-    f = open(datadir+'MRC', 'w')
-    f.close()
+    # f = open(datadir+'MRC', 'w')
+    # f.close()
 
     # Start the main loop for the Lambda iteration
-   #for itteration in tqdm(range(cdt.max_iter), desc='Lambda itteration progress'):
+    #for itteration in tqdm(range(cdt.max_iter), desc='Lambda itteration progress'):
     for itteration in range(cdt.max_iter):
 
-        # Debug
-        if debug:
-            print(f'Starting iteration {itteration+1}')
+
+        print(f'Starting iteration {itteration+1}')
 
         # Reset the internal state for a new itteration
         st.new_itter()
@@ -203,8 +86,8 @@ def main():
                 iz0 = 0
                 iz1 = cdt.z_N
 
-            # Debug
-            if debug:
+            # verbose
+            if verbose:
                 print(f'\nPropagating ray {j} {ray.inc} {ray.az}: {iz0}--{iz1}:{step}')
 
             # Deal with very first point computing first and second
@@ -219,8 +102,8 @@ def main():
                 # Set Stokes
                 point_O.setradiationas(st.space_rad)
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'Defined first point at top boundary')
 
             # If bottom boundary
@@ -229,21 +112,21 @@ def main():
                 # Set Stokes
                 point_O.setradiationas(st.sun_rad[j])
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'Defined first point at bottom boundary')
 
                 point_O.sumStokes(ray,cdt.nus_weights,cdt.JS)
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'Added Jqq contribution at point {z}')
 
             # Get RT coefficients at initial point
             sf_o, kk_o = RT_coeficients.getRTcoefs(point_O.atomic, ray, cdt)
 
-            # Debug
-            if debug:
+            # verbose
+            if verbose:
                 print(f'Got RT coefficients at point {z}')
 
             # Get next point and its RT coefficients
@@ -251,8 +134,8 @@ def main():
             point_P = point(st.atomic[z], st.radiation[z], cdt.zz[z])
             sf_p, kk_p = RT_coeficients.getRTcoefs(point_P.atomic, ray, cdt)
 
-            # Debug
-            if debug:
+            # verbose
+            if verbose:
                 print(f'Got RT coefficients at point {z}')
 
             # Go through all the points (besides 0 and -1 for being IC)
@@ -268,8 +151,8 @@ def main():
                 kk_p = None
                 sf_p = None
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'New current point {z}')
 
                 # If we are in the boundaries, compute the CL for the IC (z=0)
@@ -279,8 +162,8 @@ def main():
                     point_P = False
                     lineal = True
 
-                    # Debug
-                    if debug:
+                    # verbose
+                    if verbose:
                         print(f'Last linear point {z}')
 
                 else:
@@ -288,15 +171,15 @@ def main():
                     point_P = point(st.atomic[z+step], st.radiation[z+step], \
                                     cdt.zz[z+step])
 
-                    # Debug
-                    if debug:
+                    # verbose
+                    if verbose:
                         print(f'Prepare next point {z+step}')
 
                     # Compute the RT coeficients for the next point (for solving RTE)
                     sf_p, kk_p = RT_coeficients.getRTcoefs(point_P.atomic, ray, cdt)
 
-                    # Debug
-                    if debug:
+                    # verbose
+                    if verbose:
                         print(f'Got RT coefficients at point {z+step}')
 
                 # Propagate
@@ -306,39 +189,39 @@ def main():
                                  ray, cdt, tau_tot, not lineal, \
                                  tau, cent_limb_coef)
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'Besser {z-step}-{z}-{z+step}')
 
                 # If last point
-                if lineal:
+                # if lineal:
 
                     # If ray going out
-                    if not ray.is_downward:
+                    # if not ray.is_downward:
 
                         # Store last Stokes parameters
-                        f = open(datadir + f'stokes_{itteration:03d}_{j:02d}', 'wb')
-                        N_nus = point_O.radiation.nus.size
-                        f.write(struct.pack('i',N_nus))
-                        f.write(struct.pack('d'*N_nus,*point_O.radiation.nus))
-                        for i in range(4):
-                            f.write(struct.pack('d'*N_nus, \
-                                                *point_O.radiation.stokes[i]))
-                        f.close()
+                        # f = open(datadir + f'stokes_{itteration:03d}_{j:02d}', 'wb')
+                        # N_nus = point_O.radiation.nus.size
+                        # f.write(struct.pack('i',N_nus))
+                        # f.write(struct.pack('d'*N_nus,*point_O.radiation.nus))
+                        # for i in range(4):
+                        #     f.write(struct.pack('d'*N_nus, \
+                        #                         *point_O.radiation.stokes[i]))
+                        # f.close()
 
                         # Store optical depth
-                        f = open(datadir + f'tau_{itteration:03d}_{j:02d}', 'w')
-                        N_nus = point_O.radiation.nus.size
-                        f.write(f'{N_nus}\n')
-                        for nu,ta in zip(point_O.radiation.nus,tau):
-                            f.write(f'{1e7*c.c/nu:25.16f}  {ta:23.16e}\n')
-                        f.close()
+                        # f = open(datadir + f'tau_{itteration:03d}_{j:02d}', 'w')
+                        # N_nus = point_O.radiation.nus.size
+                        # f.write(f'{N_nus}\n')
+                        # for nu,ta in zip(point_O.radiation.nus,tau):
+                        #     f.write(f'{1e7*c.c/nu:25.16f}  {ta:23.16e}\n')
+                        # f.close()
 
                 # Add to Jqq
                 point_O.sumStokes(ray,cdt.nus_weights,cdt.JS)
 
-                # Debug
-                if debug:
+                # verbose
+                if verbose:
                     print(f'Added Jqq contribution at point {z}')
 
         # Update the MRC and check wether we reached convergence
@@ -361,8 +244,8 @@ def main():
             print('----------------------------------')
 
     # Save the rho quantities
-    io_saverho(datadir,st.atomic)
-    io_saverad(datadir,st.atomic)
+    # io_saverho(datadir,st.atomic)
+    # io_saverad(datadir,st.atomic)
 
     # Remove unused boundaries
     for j in cdt.rays:
