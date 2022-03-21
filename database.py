696a0c64d850b67c9d8563bb9e93c8ce10461773
Andres Vicente <andres.vicente.arevalo@gmail.com>
2022-03-21 13:58:07 +0100

    first working parallelization

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 # Especific modules
-from doctest import master
+from asyncio import tasks
 import parameters_rtcoefs as pm
 from conditions import conditions
 from RTcoefs import RTcoefs
@@ -8,9 +8,46 @@ from tensors import JKQ_to_Jqq, construct_JKQ_0
 
 # General modules
 from mpi4py import MPI
+import os, time, argparse
+from enum import IntEnum
+import pickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import traceback
+
+
+# Define the class to store the MPI tags
+class tags(IntEnum):
+    """ Class to define the state of a worker.
+    It inherits from the IntEnum class """
+    READY = 0
+    DONE = 1
+    EXIT = 2
+    START = 3
+
+
+# Function to check the conditions that will be used in the Rtcoefs module
+def print_params(cdts=conditions(pm), B=0):
+    ray = cdts.orays[0]
+    # print the parameters of the conditions instance
+    print('------------------------------------------------------')
+    print(f'B={B:1.3e} G')
+    print(f'computed ray:\ninclination={ray.inc}\nazimut={ray.az}\n')
+    print(f'v_dop_0={cdts.v_dop_0:1.3e}')
+    print(f'v_dop={cdts.v_dop:1.3e}')
+    print(f'a_voigt={cdts.a_voigt:1.3e}\n')
+    print(f'T={cdts.temp:1.3e}')
+    print('------------------------------------------------------\n')
+
+
+# Function to dump the data to files
+def dump_data(profiles, parameters, filename, task_index=None):
+    with open(f'{filename}_profiles.pkl', 'wb') as f:
+        pkl.dump(profiles[0:task_index], f)
+    
+    with open(f'{filename}_parameters.pkl', 'wb') as f:
+        pkl.dump(parameters[0:task_index], f)
 
 
 def new_parameters(pm):
@@ -42,26 +79,12 @@ def new_parameters(pm):
     return JKQ, JKQ, B, pm
 
 
-# Function to check the conditions that will be used in the Rtcoefs module
-def print_params(cdts=conditions(pm), B=0):
-    ray = cdts.orays[0]
-    # print the parameters of the conditions instance
-    print('------------------------------------------------------')
-    print(f'B={B:1.3e} G')
-    print(f'computed ray:\ninclination={ray.inc}\nazimut={ray.az}\n')
-    print(f'v_dop_0={cdts.v_dop_0:1.3e}')
-    print(f'v_dop={cdts.v_dop:1.3e}')
-    print(f'a_voigt={cdts.a_voigt:1.3e}\n')
-    print(f'T={cdts.temp:1.3e}')
-    print('------------------------------------------------------\n')
-
-
 # Wraper to compute the Rtcoefs module with a given parameters
 def compute_profile(JKQ_1, JKQ_2, pm=pm, B=0):
     # Initialize the conditions and rtcoefs objects
     # given a set of parameters via the parameters_rtcoefs module
     cdt = conditions(pm)
-    print_params(cdt, B)
+    # print_params(cdt, B)
     RT_coeficients = RTcoefs(cdt.nus,cdt.nus_weights,cdt.mode)
 
     # Initialize the ESE object and computing the initial populations (equilibrium = True)
@@ -107,13 +130,114 @@ def compute_profile(JKQ_1, JKQ_2, pm=pm, B=0):
     return profiles
 
 
-def master_work():
-    pass
+def master_work(nsamples, filename, write_frequency=100):
+
+    # Index of the task to keep track of each job
+    task_status = [0]*nsamples
+    task_index = 0
+    num_workers = size - 1
+    closed_workers = 0
+
+    # Initialize the variables to store the results
+    profiles = [None]*nsamples
+    parameters = [None]*nsamples
+
+    with tqdm(total=nsamples, ncols=100) as pbar:
+        while closed_workers < num_workers:
+
+            # Recieve the ready signal from the workers
+            dataReceived = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+            source = status.Get_source()
+            tag = status.Get_tag()
+
+            # If the worker is ready to compute
+            if tag == tags.READY:
+                # Look for the first task that is not done
+                try:
+                    task_index = task_status.index(0)
+                except ValueError:
+                    # If all the tasks are done, send the terminate signal to the workers
+                    task_index = -1
+
+                # Send the task to the worker
+                if task_index >= 0:
+                    comm.send(task_index, dest=source, tag=tags.START)
+                    task_status[task_index] = 1
+                else:
+                    # If the task is -1 (all the tasks done), send the kill signal
+                    comm.send(None, dest=source, tag=tags.EXIT)
+                    closed_workers += 1
+
+            if tag == tags.DONE:
+                # If the worker is done, store the results
+                task_index = dataReceived['index']
+                success = dataReceived['success']
+                
+                if success:
+                    profiles[task_index] = dataReceived['profiles']
+                    parameters[task_index] = dataReceived['parameters']
+                    task_status[task_index] = 1
+                    pbar.update(1)
+                else:
+                    task_status[task_index] = 0
+
+            # If the number of itterations is multiple with the write frequency dump the data
+            if (pbar.n / write_frequency == pbar.n // write_frequency):
+                # Dump the data
+                dump_data(profiles, parameters, filename, pbar.n)
+        
+    # Once all the workers are done, dump the data
+    print("Master finishing")
+    print("Dumping data")
+    dump_data(profiles, parameters, filename)
+    
 
 
-def slave_work():
-    pass
+def slave_work(pm):
 
+    while True:
+        # send the ready signal to the master
+        comm.send(None, dest=0, tag=tags.READY)
+
+        # Receive the task index or the kill signal from the master
+        dataReceived = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+        tag = status.Get_tag()
+
+        # if the signal is to start the computation, compute the task
+        if tag == tags.START:
+            # store the index of the received task
+            task_index = dataReceived
+            success = True
+            try:
+                # create a new parameters
+                JKQ_1, JKQ_2, B, pm = new_parameters(pm)
+                # compute the profile
+                profiles = compute_profile(JKQ_1, JKQ_2, B=B, pm=pm)
+                pm.B = B
+                pm.JKQ_1 = JKQ_1
+                pm.JKQ_2 = JKQ_2
+            except:
+                success = False
+                print('-'*50)
+                print("Error in the computation of the profile")
+                print("The parameters are:")
+                print("JKQ_1:", JKQ_1)
+                print("JKQ_2:", JKQ_2)
+                print("B:", B)
+                print("pm:", pm)
+                print("The error is:")
+                print(traceback.format_exc())
+                print('-'*50)
+                profiles = None
+
+            # Send the results to the master
+            dataToSend = {'index': task_index, 'success': success, 'profiles': profiles, 
+                          'parameters': [pm.JKQ_1, pm.JKQ_2, pm.B, pm.ray_out]}
+            comm.send(dataToSend, dest=0, tag=tags.DONE)
+        
+        # If the master is sending the kill signal exit
+        elif tag == tags.EXIT:
+            return
 
 def test_results(pm):
     print("Only one node active", flush=True)
@@ -187,8 +311,20 @@ if __name__ == '__main__':
     if size > 1:
         print(f"Node {rank}/{size} active", flush=True)
         if rank == 0:
-            master_work()
+            parser = argparse.ArgumentParser(description='Generate synthetic models and solve NLTE problem')
+            parser.add_argument('--n', '--nmodels', default=10000, type=int, metavar='NMODELS', help='Number of models')
+            parser.add_argument('--f', '--freq', default=10, type=int, metavar='FREQ', help='Frequency of model write')
+            parser.add_argument('--sav', '--savedir', default=f'data/{time.strftime("%Y%m%d-%H%M%S")}', metavar='SAVEDIR', help='directory for output files')
+
+            parsed = vars(parser.parse_args())
+
+            if not os.path.exists(parsed['sav']):
+                os.makedirs(parsed['sav'])
+            else:
+                os.system(f'rm -rf {parsed["sav"]}')
+
+            master_work(parsed['n'], parsed['sav'], parsed['f'])
         else:
-            slave_work()
+            slave_work(pm)
     else:
         test_results(pm)
