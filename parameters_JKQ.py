a9536c83f0b648a44e53c724cafef731be95219d
Andres Vicente <andres.vicente.arevalo@gmail.com>
2022-04-19 09:42:30 +0200

    2.

---
+++ parameters_JKQ.py
@@ -0,0 +1,23 @@
+import time
+
+z0 = 5010.0*1e5   # height of the slav over the surfave [cm] (around 3 arcsec)
+zf = 5000.0*1e5   # geometrical height of the slab [cm] (around 3 arcsec)
+zn = 10           # Points in z in the slab
+alpha = 0.        # angle between the vertical of the slav and the radial line [deg]
+
+ray_quad = "gaussian_quadrature_32x8.dat" # file where the angular quadrature is saved
+ray_out = [[0.05, 0.],[1.0, 0.]]             # List of mu,phi for emergent radiation directions
+
+v_dop = 5.0*1e5            # Dopler velocity (not the actual one) [cm/s]
+a_voigt = 1e-99            # voigt damping parameter of the line profile
+n_dens = 5.8e4             # Density wich defines the optical thickness [cm^-3]
+temp = 8.665251563142749e3 # Temperature of the slab [K] (6 km/s)
+Trad = 6e3                 # Black body star temperature for radiation [K]
+
+max_iter = 100       # Maximum itterations for the forward method
+tolerance_p = 5e-6   # tolerated relative change in populations
+tolerance_c = 5e-4   # tolerated relative change in coherences
+
+initial_equilibrium = True   # Start from equilibrium
+
+dir = f'output_{time.strftime("%Y%m%d-%H%M%S")}/'
