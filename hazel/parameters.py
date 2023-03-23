2e7950d248eeef914fd872016cd348f80a82bec6
Andres Vicente <andres.vicente.arevalo@gmail.com>
2023-03-23 16:27:10 +0000

    changed conditions added hazel params and changed atmos

---
+++ hazel/parameters.py
@@ -0,0 +1,32 @@
+import time
+
+z0 = 30000.0*1e5   # height of the slav over the surfave [cm]
+zf = 30240.0*1e5   # geometrical height of the slab [cm]
+zn = 10            # Points in z in the slab
+
+ray_quad = "quadrature_gaussian_32x8.dat"# file where the angular quadrature is saved
+ray_out = [[1.0,0.]]             # List of mu,phi for emergent radiation directions
+
+v_dop = 5.0*1e5            # Dopler velocity (not the actual one) [cm/s]
+a_voigt = 1e-2             # voigt damping parameter of the line profile
+n_dens = 5.8e4             # Density wich defines the optical thickness [cm^-3]
+temp = 1.5e4               #8.665251563142749e3 # Temperature of the slab [K] (6 km/s)
+
+B = 10                     # Magnetic field strength [T]
+B_inc = 90.0               # Magnetic field inclination [deg]
+B_az = 0.0                 # Magnetic field azimuth [deg]
+
+velocity = [0., 0., 0.]   # Velocity of the slab [cm/s]
+
+max_iter = 200       # Maximum itterations for the forward method
+tolerance_p = 5e-6   # tolerated relative change in populations
+tolerance_c = 5e-4   # tolerated relative change in coherences
+
+initial_equilibrium = True   # Start from equilibrium
+verbose = False
+extra_plots = False
+extra_save = True
+especial = True
+
+dir = f'output_dz_{(zf-z0)/1e5:1.1f}_B_{B}_sp_{especial}_qd_{ray_quad[-8:-4]}_{time.strftime("%Y%m%d-%H%M%S")}/'
+del time
